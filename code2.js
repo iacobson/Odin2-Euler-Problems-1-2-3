8c54b1fe5276b962ab6fdf4f92e6b5567566579a
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2014-09-28 01:13:17 +0300

    Problem 2
    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

---
+++ code2.js
@@ -0,0 +1,27 @@
+var problem2 = function () {
+	var fibo1 = 1;
+	var fibo2 = 1;
+	var toteven1 = 0;
+	var toteven2 = 0;
+	var totevengen = 0;
+	var limit2 = document.getElementById('limithtml2');
+
+		for (fibo1 = 1; fibo1 < limit2.value && fibo2 < limit2.value; fibo1 = fibo1 + fibo2) {  // IMPORTANT !!! add ".value" otherwise to generate number. Otherwise will be zero
+			fibo2 = fibo1 + fibo2;
+			if (fibo1 % 2 === 0) {
+				toteven1 += fibo1;
+				if (fibo2 % 2 === 0) {
+					toteven2 += fibo2;
+				}
+			}
+			else if (fibo1 % 2 !== 0 && fibo2 % 2 === 0) {
+				toteven2 += fibo2;
+			}
+		}
+	totevengen = toteven1 + toteven2;
+	
+	console.log("fibolimit" + limit2);
+	console.log("fibo1" + toteven1);
+	console.log("fibo2" + toteven2);
+	console.log("fiboeven" + totevengen);
+}
